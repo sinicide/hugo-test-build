0485186b90b2fcd50f1eda9c95f2d991ad9e9eef
Huy Nguyen <hello@huynguyen.me>
2023-03-07 23:39:07 -0600

    Added some additional hugo build options

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # Hugo Live Server
+#env HUGO_googleAnalytics="hello_world" \
 /bin/hugo server --watch \
     --buildDrafts \
     --disableFastRender \
+    --noHTTPCache \
+    --ignoreCache \
     --bind 0.0.0.0 \
     --port $HUGO_PORT \
     --environment $HUGO_BUILD_ENVIRONMENT \
